4964aa3d94d422b0f0aa6de4c580387a1b497912
Arshath <arshathhaseen@gmail.com>
2025-03-29 11:25:27 +0530

    after some section update5

--- src/Components/Comparission.tsx
+++ src/Components/Comparission.tsx
@@ -10,7 +10,7 @@ const comparisonData = [
 
 function Comparison() {
   return (
-    <section className="p-24 bg-black">
+    <section className="py-8 bg-black">
       <div className="max-w-7xl mx-auto relative overflow-hidden rounded-2xl bg-gray-800 p-10">
         <h2 className="text-5xl text-white font-bold text-center mb-8">The Roar Difference</h2>
 
